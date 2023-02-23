e3f4d8a2a0c680a6109943347344a5ff2cbddc4f
AlexKarp <karpen_ko@hotmail.com>
2023-02-23 19:34:21 +0100

    plausichecks can be produced

---
+++ controllers/jobs/PlausiIssueProducer.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Job for resolving BIS issues
+ */
+class PlausiIssueProducer extends PlausiIssueProducer_Controller
+{
+	protected $_extensionName = 'FHC-Core-BIS'; // name of extension for file path
+
+	public function __construct()
+	{
+		parent::__construct();
+
+		// set fehler which can be produced by the job
+		// structure: fehler_kurzbz => class (library) name for resolving
+		$this->_fehlerLibMappings = array(
+			'uhstatStaatsbuergerschaftFehlt' => 'uhstatStaatsbuergerschaftFehlt'
+		);
+	}
+}
---
+++ libraries/issues/plausichecks/uhstatStaatsbuergerschaftFehlt.php
@@ -0,0 +1,70 @@
+<?php
+
+if (! defined('BASEPATH')) exit('No direct script access allowed');
+
+//require_once('PlausiChecker.php');
+require_once APPPATH.'libraries/issues/plausichecks/PlausiChecker.php';
+
+/**
+ * Staatsbürgerschaft missing
+ */
+class uhstatStaatsbuergerschaftFehlt extends PlausiChecker
+{
+	public function executePlausiCheck($params)
+	{
+		$results = array();
+
+		if (!isset($params['studiensemester_kurzbz']) || isEmptyString($params['studiensemester_kurzbz']))
+			return error('Studiensemester missing, issue_id: '.$params['issue_id']);
+
+		$this->_ci->load->library('extensions/FHC-Core-BIS/JQMSchedulerLib');
+		$this->_ci->config->load('extensions/FHC-Core-BIS/BISSync');
+
+		$params = array($params['studiensemester_kurzbz'], $this->_ci->config->item('fhc_bis_status_kurzbz')[JQMSchedulerLib::JOB_TYPE_UHSTAT0]);
+
+		// get students
+		$qry = "SELECT
+					DISTINCT prestudent_id, studiensemester_kurzbz
+				FROM
+					public.tbl_prestudent ps
+					JOIN public.tbl_prestudentstatus pss USING (prestudent_id)
+				WHERE
+					studiensemester_kurzbz = ?
+					AND status_kurzbz IN ?
+					AND EXISTS ( /* is registered for Reihungstest */
+						SELECT 1
+						FROM
+							public.tbl_rt_person rtp
+							JOIN tbl_reihungstest rt ON(rtp.rt_id = rt.reihungstest_id)
+						WHERE
+							AND rtp.person_id = ps.person_id
+							AND rt.studiensemester_kurzbz = pss.studiensemester_kurzbz
+					)";
+
+		$dbModel = new DB_Model();
+
+		$studResult = $dbModel->execReadOnlyQuery(
+			$qry,
+			$params
+		);
+
+		// If error occurred while retrieving students from database then return the error
+		if (isError($studResult)) return $studResult;
+
+		// If students are present
+		if (hasData($studResult))
+		{
+			$prestudents = getData($studRes);
+
+			// populate results with data necessary for writing issues
+			foreach ($prestudents as $prestudent)
+			{
+				$results[] = array(
+					'person_id' => $prestudent->person_id
+				);
+			}
+		}
+
+		return success($results);
+	}
+}

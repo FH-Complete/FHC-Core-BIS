78290cdfd8a4dcdb19c16afbab482390aca9e744
Harald Bamberger <harald.bamberger@technikum-wien.at>
2024-03-25 15:28:59 +0100

    rename Menuentries injected into PV21

--- config/Events.php
+++ config/Events.php
@@ -5,11 +5,11 @@ use CI3_Events as Events;
 
 Events::on('pv21_conf_stammdaten', function (&$res) {
 	$res()['bis_hauptberuf'] = [
-		'title' => 'BIS Daten Hauptberuf',
+		'title' => 'BIS Hauptberuf',
 		'component' => '../../extensions/FHC-Core-BIS/js/components/Personalmeldung/Hauptberuf.js'
 	];
 	$res()['bis_verwendung'] = [
-		'title' => 'BIS Daten Verwendung',
+		'title' => 'BIS Verwendung',
 		'component' => '../../extensions/FHC-Core-BIS/js/components/Personalmeldung/Verwendungen.js'
 	];
 });

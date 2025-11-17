008b9b282d68c11d5c5e64eccf64b800f92b2efd
Alexei Karpenko <karpen_ko@hotmail.com>
2025-11-17 18:40:37 +0100

    personalverwaltung menu links: using absoluteJsImportUrl method

--- config/Events.php
+++ config/Events.php
@@ -6,10 +6,10 @@ use CI3_Events as Events;
 Events::on('pv21_conf_stammdaten', function (&$res) {
 	$res()['bis_hauptberuf'] = [
 		'title' => 'BIS Hauptberuf',
-		'component' => APP_ROOT . 'public/extensions/FHC-Core-BIS/js/components/Personalmeldung/Hauptberuf.js',
+		'component' => absoluteJsImportUrl('public/extensions/FHC-Core-BIS/js/components/Personalmeldung/Hauptberuf.js'),
 	];
 	$res()['bis_verwendung'] = [
 		'title' => 'BIS Verwendung',
-		'component' => APP_ROOT . 'public/extensions/FHC-Core-BIS/js/components/Personalmeldung/Verwendungen.js',
+		'component' => absoluteJsImportUrl('public/extensions/FHC-Core-BIS/js/components/Personalmeldung/Verwendungen.js'),
 	];
 });
